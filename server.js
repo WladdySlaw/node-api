58c973e2254ad1791cef5d7dd4478ee0b8ffd556
wladdyslaw <wladdyslaw@MacBook-Pro-wladdyslaw.local>
2017-04-19 20:37:14 +0300

    my first node api

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var ObjectID = require('mongodb').ObjectID;
+
+var app = express();
+var db = require('./db');
+
+var artistsController = require('./controllers/artists');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', function(req, res){
+    res.send('Hello Api');
+});
+
+app.get('/artists', artistsController.all);
+
+app.get('/artists/:id', artistsController.findById);
+
+app.post('/artists', artistsController.create);
+
+app.put('/artists/:id', artistsController.update);
+
+app.delete('/artists/:id', artistsController.delete);
+
+db.connect('mongodb://localhost:27017/myapi', function(err){
+    if (err){
+        return console.log(err);
+    }
+    app.listen(3012, function(){
+        console.log('API app started');
+    });
+});
